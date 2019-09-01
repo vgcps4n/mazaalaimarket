1668632236e37c35dbc237f450b5dd592bda67df
vgcps4n <prizo.crow@gmail.com>
2019-09-02 01:19:29 +0800

    Updates

---
+++ precache-manifest.e1f14142a46df0fe7d3292df36c4bc98.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb6f4928e90325ab9cc00715ae03923",
+    "url": "/mazaalaimarket/index.html"
+  },
+  {
+    "revision": "d534e86539206f525717",
+    "url": "/mazaalaimarket/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "22a47d120e966a5b41ee",
+    "url": "/mazaalaimarket/static/css/main.84ca8a52.chunk.css"
+  },
+  {
+    "revision": "d534e86539206f525717",
+    "url": "/mazaalaimarket/static/js/2.ffa7283f.chunk.js"
+  },
+  {
+    "revision": "22a47d120e966a5b41ee",
+    "url": "/mazaalaimarket/static/js/main.cdb44b4c.chunk.js"
+  },
+  {
+    "revision": "691d05253469e80c9948",
+    "url": "/mazaalaimarket/static/js/runtime~main.21a3f555.js"
+  },
+  {
+    "revision": "8b69af64cfbc4527fa1f431a1c8bb888",
+    "url": "/mazaalaimarket/static/media/bg.8b69af64.jpg"
+  },
+  {
+    "revision": "42497fb30dbdc61ccb8d59fa8c27a1f2",
+    "url": "/mazaalaimarket/static/media/brand.42497fb3.svg"
+  },
+  {
+    "revision": "fef593c92fe590629d305f301824356a",
+    "url": "/mazaalaimarket/static/media/logo.fef593c9.png"
+  },
+  {
+    "revision": "e1f5f591d0b488518b5a38d5dbad8e22",
+    "url": "/mazaalaimarket/static/media/logopic.e1f5f591.png"
+  },
+  {
+    "revision": "d3eaf389e048b72efe4330ad9c36506b",
+    "url": "/mazaalaimarket/static/media/logoword.d3eaf389.png"
+  },
+  {
+    "revision": "adb442a55a1471b049535b9d876fc1ca",
+    "url": "/mazaalaimarket/static/media/logowordblack.adb442a5.png"
+  },
+  {
+    "revision": "8e1ae1b2e682cf375d9746555191a45c",
+    "url": "/mazaalaimarket/static/media/mazaalai-1.8e1ae1b2.svg"
+  },
+  {
+    "revision": "adf6b52c04ba897e0dd13fb8f1e6d1ea",
+    "url": "/mazaalaimarket/static/media/mazaalai-2.adf6b52c.svg"
+  },
+  {
+    "revision": "88d8ee14a4d2af5ab5875f9d1e6b5a0f",
+    "url": "/mazaalaimarket/static/media/zorilt.88d8ee14.svg"
+  }
+]);
